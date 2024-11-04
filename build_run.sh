83ccf32887553b7c706057a991e8b2af54a12ad6
Djangoboyy <romanshevtsovcoderman@yandex.ru>
2024-11-05 00:33:33 +0300

    Выполненное задание

--- build_run.sh
+++ build_run.sh
@@ -5,7 +5,7 @@ CHARSET="-finput-charset=utf-8 -fexec-charset=utf-8"
 
 # Определение имен файлов
 CPP_FILES=*.cpp
-PRODUCT="prog"
+PRODUCT="prog.exec"
 
 # Удаление предыдущего исполняемого файла, если он существует
 if [ -f "$PRODUCT" ]; then
